8e760a51818695005fb2c46dd3b51f323ed1e86a
Akshat Adsule <30205531+AkshatAdsule@users.noreply.github.com>
2024-12-15 20:55:22 -0800

    add smore c/cpp packages

--- pkgs/dev.nix
+++ pkgs/dev.nix
@@ -7,14 +7,29 @@
     vim
     neovim
 
-    # build-esstential
+    # [C/C++] Development
     gcc
     glibc
-    make
-    cmake
+    gnumake
     autoconf
     automake
     libtool
     llvmPackages_19.clang-unwrapped
+
+    gmp gmp.dev
+    isl
+    libffi libffi.dev
+    libmpc
+    libxcrypt
+    mpfr mpfr.dev
+    xz xz.dev
+    zlib zlib.dev
+
+    m4
+    bison
+    flex
+    texinfo
+
+    valgrind
   ];
 }
