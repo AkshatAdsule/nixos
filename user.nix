dbf98eca3c8cb91e42963b7fb1ba2be7c3f3a738
Akshat Adsule <30205531+AkshatAdsule@users.noreply.github.com>
2024-12-15 18:00:01 -0800

    add home-manager configuration

--- user.nix
+++ user.nix
@@ -1,9 +1,16 @@
-{lib, config, pkgs, ... }:
+{lib, config, pkgs, inputs, ... }:
 
 {
   users.users.akshat = {
     isNormalUser = true;
     description = "Akshat Adsule";
     extraGroups = [ "networkmanager" "wheel" ];
+  };
+
+  home-manager = {
+    specialArgs = { inherit inputs; };
+    users = {
+      "akshat" = import ./home.nix;
+    };
   };
 }
