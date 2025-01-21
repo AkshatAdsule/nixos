822cd366c2bace6fed6a91cebf125c70fa226d39
Akshat Adsule <30205531+AkshatAdsule@users.noreply.github.com>
2025-01-21 11:28:08 -0800

    update flake

--- flake.nix
+++ flake.nix
@@ -8,19 +8,31 @@
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
+    oskars-dotfiles = {
+      url = "github:oskardotglobal/.dotfiles/nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = {
     self,
     nixpkgs,
+    oskars-dotfiles,
     ...
   } @ inputs: {
     nixosConfigurations.default = nixpkgs.lib.nixosSystem {
       specialArgs = {inherit inputs;};
       modules = [
         ./configuration.nix
+        ({pkgs, ...}: {
+          nixpkgs.overlays = [oskars-dotfiles.overlays.spotx];
+          environment.systemPackages = [pkgs.spotify];
+        })
         inputs.home-manager.nixosModules.default
       ];
     };
+
+
   };
 }
